1e081826530d36b2a0970fe64f28039087f4572b
assykot48 <assykot48@gmail.com>
2026-06-30 17:45:29 +0500

    Add health check API endpoint

---
+++ app/Http/Controllers/Api/HealthController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HealthController extends Controller
+{
+    public function index() {
+        return response()->json([
+            'status' => 'ok',
+            'app' => 'MyBudget',
+            'version' => '0.1.0'
+        ]);
+    }
+}
